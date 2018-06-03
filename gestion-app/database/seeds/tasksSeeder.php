5a2695971d4d0769e4ddc7d19693388c23fdf4de
TheoC0m <theo.c.developer@gmail.com>
2018-06-03 15:42:35 +0200

    created empty db Seeder files

---
+++ gestion-app/database/seeds/tasksSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class tasksSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
